5aab0114d6944e0ca9a8993b8d88f249735e45b3
Arthur Zhu <arthurzhu29@gmail.com>
2024-02-12 00:30:11 +0100

    Modify \"index.php\" head element body
    Add boilerplat and icon.

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="/repo/assets/images/icon.png">
+    <title>Testing!!!</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
