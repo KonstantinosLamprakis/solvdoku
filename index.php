24fa4ce4ad948856bfc582a535695e4f87b5f90d
Arthur Zhu <arthurzhu29@gmail.com>
2024-02-12 00:21:00 +0100

    Create working test using solving algorithm
    The first working test accepts user input, but is not yet made secure.
    It will solve the sudoku and display the result. It will display the
    first found solution, or \"no solution found\" if no solution is found.
    
    Make the user input form pretty with our first css file. Inspiration:
    sudokuspoiler.com.

--- index.php
+++ index.php
@@ -1,12 +1,114 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="/repo/assets/images/icon.png">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
+    <form method="GET" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <div id="grid"><input class="top left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="right top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><br> <input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="right" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><br> <input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="right" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><br> <input class="top left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="right top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><br> <input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="right" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><br> <input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="right" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><br> <input class="top left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="right top" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><br> <input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="right" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><br> <input class="bottom left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="bottom" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="bottom" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="bottom left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="bottom" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="bottom" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="bottom left" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="bottom" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><input class="bottom right" type="text" autocomplete="off" maxlength="1" name="data[]"
+                inputmode="numeric"><br>
+        </div>
+        <input type="submit" name="submit" value="Submit"><br>
+        <!-- <?php var_dump($_GET["data"]); ?> -->
+    </form>
+    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            $i = 0;
+            for ($y = 0; $y < 9; $y++) {
+                for ($x = 0; $x < 9; $x++) {
+                    $grid[$y][$x] = (int) $_GET["data"][$i++];
+                }
+            }
+        } else {
+            $grid = false;
+        }
+    ?>
     <?php include("solvdoku-core.php"); ?>
+    <form method="POST" action="index.php">
+        <input type="submit" name="reset" value="Reset">
+    </form>
 </body>
+
 </html>
